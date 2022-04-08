ce2c6edf23f5fbad6996bda303533f35292a607f
motiejus1 <motiejus.baliukonis@vka.lt>
2022-04-08 20:55:58 +0300

    *portfolio page

--- page-kontaktai.php
+++ page-kontaktai.php
@@ -1,28 +1,35 @@
+<?php 
+
+$contact_form_shortcode = get_field('contact_form_shortcode');
+
+$contact_title = get_field('contact_title');
+$contact_address = get_field('contact_address');
+$contact_phone = get_field('contact_phone');
+$contact_email = get_field('contact_email');
+
+?>
 <?php get_header(); ?>
 		<div id="colorlib-main">
 			<section class="ftco-section contact-section">
 	      <div class="container">
 	        <div class="row d-flex mb-5 contact-info">
 	          <div class="col-md-12 mb-4">
-	            <h2 class="h4 font-weight-bold">Contact Information</h2>
+	            <h2 class="h4 font-weight-bold"><?php echo $contact_title ?></h2>
 	          </div>
 	          <div class="w-100"></div>
 	          <div class="col-md-3">
-	            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+	            <p><span>Address:</span> <?php echo $contact_address ?></p>
 	          </div>
 	          <div class="col-md-3">
-	            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+	            <p><span>Phone:</span> <?php echo $contact_phone; ?></p>
 	          </div>
 	          <div class="col-md-3">
-	            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-	          </div>
-	          <div class="col-md-3">
-	            <p><span>Website</span> <a href="#">yoursite.com</a></p>
+	            <p><span>Email:</span> <?php echo $contact_email; ?></p>
 	          </div>
 	        </div>
 	        <div class="row block-9">
 	          <div class="col-md-6 order-md-last pr-md-5">
-	            <form action="#">
+	            <!-- <form action="#">
 	              <div class="form-group">
 	                <input type="text" class="form-control" placeholder="Your Name">
 	              </div>
@@ -38,12 +45,13 @@
 	              <div class="form-group">
 	                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
 	              </div>
-	            </form>
+	            </form> -->
+
+                <?php echo do_shortcode( $contact_form_shortcode ); ?>
 	          
 	          </div>
 
 	          <div class="col-md-6">
-	          	<div id="map"></div>
 	          </div>
 	        </div>
 	      </div>
--- page-portfolio.php
+++ page-portfolio.php
@@ -3,114 +3,38 @@
 			<section class="ftco-section-2">
 				<div class="photograhy">
 					<div class="row no-gutters">
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_1.jpg);">
+
+                        <?php 
+                        
+                            $query = new WP_Query(array(
+                                'post_type' => 'works'
+                            ));
+                        
+                            if($query->have_posts()) :
+                        ?>
+                            <?php while($query->have_posts()): 
+                                $query->the_post();
+
+                                $work_image = get_field("image_field", get_the_ID() );
+                                $image_size = 'thumbnail';
+                                ?>
+                               
+                                <div class="col-md-3">
+                                <a href="#" class="photography-entry img d-flex justify-content-center align-items-center" 
+                                style="background-image: url(<?php echo $work_image['url'] ?>);">
 								<div class="overlay"></div>
 								<div class="text text-center">
-									<h3>Work 01</h3>
-									<span>Technology</span>
+									<h3><?php the_title(); ?></h3>
+									<span><?php echo get_field('git_hub_url', get_the_ID()); ?></span> 
+                                    <!-- githubo url -->
 								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_2.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 02</h3>
-									<span>Nature</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_3.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 03</h3>
-									<span>Fashion</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_4.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 04</h3>
-									<span>Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_5.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 05</h3>
-									<span>Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_6.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 06</h3>
-									<span>Travel</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_7.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 07</h3>
-									<span>Fashion, Model</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_8.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 08</h3>
-									<span>Nature</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_9.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 09</h3>
-									<span>Technology</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_10.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 10</h3>
-									<span>Model</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_11.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 11</h3>
-									<span>Fashion</span>
-								</div>
-							</a>
-						</div>
-						<div class="col-md-3">
-							<a href="#" class="photography-entry img d-flex justify-content-center align-items-center" style="background-image: url(images/image_12.jpg);">
-								<div class="overlay"></div>
-								<div class="text text-center">
-									<h3>Work 12</h3>
-									<span>Photography</span>
-								</div>
-							</a>
-						</div>
+                                </a>
+						    </div> 
+                            <?php endwhile; ?>
+                            <?php wp_reset_postdata();?>    
+
+
+                         <?php endif; ?>       
 					</div>
 				</div>
 			</section>
